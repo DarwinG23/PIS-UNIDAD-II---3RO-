f13439d3e00c8fcdaf7e411e3acc79fc09781508
Abel214 <abel.mora@unl.edu.ec>
2023-12-30 15:21:51 -0500

    Modificación de la vista docente tarea, además, implementación JcCalendar

--- PIS UNIDAD II/src/tareas/vista/TareaDocente.java
+++ PIS UNIDAD II/src/tareas/vista/TareaDocente.java
@@ -4,12 +4,14 @@
  */
 package tareas.vista;
 
+import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import javax.swing.table.TableModel;
 import tareas.controlador.controladorTarea.administrarTarea1;
 import tareas.vista.modeloTabla.modeloTablaTarea;
 import tareas.vista.utilVista.utilVistaEstadoTarea;
 import tareas.vista.utilVista.utilVistaTipoTarea;
+
 
 /**
  *
@@ -65,6 +67,8 @@ public class TareaDocente extends javax.swing.JFrame {
             tareaControl.getTarea1().setId_estado(utilVistaEstadoTarea.ObtenerEstadoTarea(cbxEstado));
             tareaControl.getTarea1().setNota(Float.parseFloat(txtNota.getText()));
             tareaControl.getTarea1().setTituloTarea(txtTitulo.getText());
+            tareaControl.getTarea1().setFechaInicio(txtFechaInicio.getText());
+            tareaControl.getTarea1().setFechaFinal(txtFechaFInal.getText());
             if (tareaControl.persist()) {
                 JOptionPane.showMessageDialog(null, "Datos guardados con exito");
                 tareaControl.setTarea1(null);
@@ -88,8 +92,10 @@ public class TareaDocente extends javax.swing.JFrame {
                 txtDescripcion.setText(tareaControl.getTarea1().getDescripcion());
                 txtNota.setText(String.valueOf(tareaControl.getTarea1().getNota()));
                 txtTitulo.setText(tareaControl.getTarea1().getTituloTarea());
-                cbxEstado.setSelectedItem(tareaControl.getTarea1().getId_tarea());
-
+                txtFechaInicio.setText(tareaControl.getTarea1().getFechaInicio());
+                txtFechaFInal.setText(tareaControl.getTarea1().getFechaFinal());
+                cbxEstado.setSelectedItem(tareaControl.getTarea1().getId_estado());
+                cbxTipo.setSelectedItem(tareaControl.getTarea1().getId_tarea());
             } catch (Exception e) {
             }
 
@@ -117,11 +123,16 @@ public class TareaDocente extends javax.swing.JFrame {
         txtNota = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblMostrar = new javax.swing.JTable();
-        btnFecha = new javax.swing.JButton();
         btnGuardar = new javax.swing.JButton();
         jLabel7 = new javax.swing.JLabel();
         btnSeleccionar = new javax.swing.JButton();
         btnModificar = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        txtFechaInicio = new javax.swing.JTextField();
+        txtFechaFInal = new javax.swing.JTextField();
+        jCalendar1 = new com.toedter.calendar.JCalendar();
+        jCalendar2 = new com.toedter.calendar.JCalendar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -133,33 +144,33 @@ public class TareaDocente extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Ebrima", 3, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 153, 153));
         jLabel1.setText("TAREAS");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 10, 155, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 10, 155, -1));
 
         jLabel2.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(153, 0, 153));
         jLabel2.setText("TÍTULO TAREA:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 78, -1, -1));
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 100, -1, -1));
 
         txtTitulo.setBackground(new java.awt.Color(204, 204, 255));
-        jPanel1.add(txtTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 80, 341, -1));
+        jPanel1.add(txtTitulo, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 80, 230, 50));
 
         jLabel3.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(153, 0, 153));
         jLabel3.setText("TIPO TAREA:");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(25, 141, -1, -1));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, -1, -1));
 
         cbxTipo.setBackground(new java.awt.Color(204, 204, 255));
         cbxTipo.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         cbxTipo.setForeground(new java.awt.Color(0, 0, 0));
-        jPanel1.add(cbxTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(171, 136, 342, -1));
+        jPanel1.add(cbxTipo, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 144, 230, 40));
 
         jLabel4.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(153, 0, 153));
         jLabel4.setText("DESCRIPCIÓN TAREA");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 170, 200, 50));
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, 200, 50));
 
         txtDescripcion.setBackground(new java.awt.Color(204, 204, 255));
-        jPanel1.add(txtDescripcion, new org.netbeans.lib.awtextra.AbsoluteConstraints(32, 210, 480, 79));
+        jPanel1.add(txtDescripcion, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 240, 410, 70));
 
         jLabel5.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(153, 0, 153));
@@ -168,15 +179,15 @@ public class TareaDocente extends javax.swing.JFrame {
 
         cbxEstado.setBackground(new java.awt.Color(204, 204, 255));
         cbxEstado.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
-        jPanel1.add(cbxEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 340, 342, -1));
+        jPanel1.add(cbxEstado, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 330, 220, 40));
 
         jLabel6.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(153, 0, 153));
         jLabel6.setText("NOTA DE LA TAREA:");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 420, -1, -1));
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 400, -1, -1));
 
         txtNota.setBackground(new java.awt.Color(204, 204, 255));
-        jPanel1.add(txtNota, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 420, 310, -1));
+        jPanel1.add(txtNota, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 400, 220, -1));
 
         tblMostrar.setBackground(new java.awt.Color(204, 255, 204));
         tblMostrar.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -191,12 +202,7 @@ public class TareaDocente extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tblMostrar);
 
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(531, 124, -1, 263));
-
-        btnFecha.setBackground(new java.awt.Color(255, 255, 255));
-        btnFecha.setForeground(new java.awt.Color(255, 153, 153));
-        btnFecha.setText("ASIGANR FECHAS PARA LA TAREA");
-        jPanel1.add(btnFecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(536, 74, -1, -1));
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 520, 580, 120));
 
         btnGuardar.setBackground(new java.awt.Color(255, 255, 255));
         btnGuardar.setForeground(new java.awt.Color(255, 153, 153));
@@ -206,12 +212,12 @@ public class TareaDocente extends javax.swing.JFrame {
                 btnGuardarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(827, 74, -1, -1));
+        jPanel1.add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 460, -1, -1));
 
         jLabel7.setFont(new java.awt.Font("Dialog", 3, 14)); // NOI18N
         jLabel7.setForeground(new java.awt.Color(204, 0, 51));
         jLabel7.setText("¿DESEA CAMBIAR ALGUN DATO?");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 390, -1, -1));
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 520, -1, -1));
 
         btnSeleccionar.setBackground(new java.awt.Color(255, 255, 255));
         btnSeleccionar.setForeground(new java.awt.Color(255, 153, 204));
@@ -221,7 +227,7 @@ public class TareaDocente extends javax.swing.JFrame {
                 btnSeleccionarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnSeleccionar, new org.netbeans.lib.awtextra.AbsoluteConstraints(565, 416, -1, -1));
+        jPanel1.add(btnSeleccionar, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 550, -1, -1));
 
         btnModificar.setBackground(new java.awt.Color(255, 255, 255));
         btnModificar.setForeground(new java.awt.Color(255, 153, 204));
@@ -231,17 +237,48 @@ public class TareaDocente extends javax.swing.JFrame {
                 btnModificarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnModificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(794, 416, -1, -1));
+        jPanel1.add(btnModificar, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 590, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(153, 0, 153));
+        jLabel8.setText("FECHA INICIO:");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 80, -1, -1));
+
+        jLabel9.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(153, 0, 153));
+        jLabel9.setText("FECHA FINAL:");
+        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 290, -1, -1));
+        jPanel1.add(txtFechaInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 80, 200, -1));
+        jPanel1.add(txtFechaFInal, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 290, 200, -1));
+
+        jCalendar1.setFont(new java.awt.Font("Dialog", 0, 10)); // NOI18N
+        jCalendar1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                jCalendar1PropertyChange(evt);
+            }
+        });
+        jPanel1.add(jCalendar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 320, 390, 160));
+
+        jCalendar2.setFont(new java.awt.Font("Dialog", 0, 10)); // NOI18N
+        jCalendar2.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                jCalendar2PropertyChange(evt);
+            }
+        });
+        jPanel1.add(jCalendar2, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 110, 390, 160));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1003, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 886, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 498, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 644, Short.MAX_VALUE)
+                .addGap(14, 14, 14))
         );
 
         pack();
@@ -262,6 +299,8 @@ public class TareaDocente extends javax.swing.JFrame {
             tareaControl.getTarea1().setId_estado(utilVistaEstadoTarea.ObtenerEstadoTarea(cbxEstado));
             tareaControl.getTarea1().setNota(Float.parseFloat(txtNota.getText()));
             tareaControl.getTarea1().setTituloTarea(txtTitulo.getText());
+            tareaControl.getTarea1().setFechaInicio(txtFechaInicio.getText());
+            tareaControl.getTarea1().setFechaFinal(txtFechaFInal.getText());
             if (tareaControl.marge(tareaControl.getTarea1(), tblMostrar.getSelectedRow())) {
                 JOptionPane.showMessageDialog(null, "Datos guardados con exito");
                 tareaControl.setTarea1(null);
@@ -275,6 +314,20 @@ public class TareaDocente extends javax.swing.JFrame {
 
         }
     }//GEN-LAST:event_btnModificarActionPerformed
+
+    private void jCalendar2PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jCalendar2PropertyChange
+        if(evt.getOldValue()!=null){
+           SimpleDateFormat ft=new SimpleDateFormat("dd/MM/yyyy");
+           txtFechaInicio.setText(ft.format(jCalendar2.getCalendar().getTime()));
+       }
+    }//GEN-LAST:event_jCalendar2PropertyChange
+
+    private void jCalendar1PropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_jCalendar1PropertyChange
+       if(evt.getOldValue()!=null){
+           SimpleDateFormat ft=new SimpleDateFormat("dd/MM/yyyy");
+           txtFechaFInal.setText(ft.format(jCalendar1.getCalendar().getTime()));
+       }
+    }//GEN-LAST:event_jCalendar1PropertyChange
 
     /**
      * @param args the command line arguments
@@ -312,12 +365,13 @@ public class TareaDocente extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnFecha;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnModificar;
     private javax.swing.JButton btnSeleccionar;
     private javax.swing.JComboBox<String> cbxEstado;
     private javax.swing.JComboBox<String> cbxTipo;
+    private com.toedter.calendar.JCalendar jCalendar1;
+    private com.toedter.calendar.JCalendar jCalendar2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -325,10 +379,14 @@ public class TareaDocente extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblMostrar;
     private javax.swing.JTextField txtDescripcion;
+    private javax.swing.JTextField txtFechaFInal;
+    private javax.swing.JTextField txtFechaInicio;
     private javax.swing.JTextField txtNota;
     private javax.swing.JTextField txtTitulo;
     // End of variables declaration//GEN-END:variables
