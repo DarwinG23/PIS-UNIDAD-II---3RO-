0cb602c58d4c8d4245101082c6df7a2cd9a9d5d8
Darwin Granda <darwin.granda@unl.edu.ec>
2023-12-29 20:33:48 -0500

    Preparacacion lista, dao, libs, flies

---
+++ PIS UNIDAD II/src/exeption/EmptyException.java
@@ -0,0 +1,28 @@
+package exeption;
+
+
+
+
+/**
+ *
+ * @author darwi
+ */
+public class EmptyException  extends Exception{
+
+    /**
+     * Creates a new instance of <code>EmptyException</code> without detail
+     * message.
+     */
+    public EmptyException() {
+    }
+
+    /**
+     * Constructs an instance of <code>EmptyException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public EmptyException(String msg) {
+        super(msg);
+    }
+}
