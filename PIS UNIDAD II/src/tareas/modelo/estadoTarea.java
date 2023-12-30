dfbd4e179dfa69d5a142f2a108e007d85c54fcec
Abel214 <abel.mora@unl.edu.ec>
2023-12-29 22:07:00 -0500

    Codigo para clases modelo

--- PIS UNIDAD II/src/tareas/modelo/estadoTarea.java
+++ PIS UNIDAD II/src/tareas/modelo/estadoTarea.java
@@ -9,5 +9,36 @@ package tareas.modelo;
  * @author ALEJANDRO
  */
 public class estadoTarea {
-    
+    private String estado;
+    private Integer Id;
+
+    public estadoTarea(Integer Id,String estado) {
+        this.estado = estado;
+
+        this.Id = Id;
+    }
+    public estadoTarea(){
+        
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    @Override
+    public String toString() {
+        return estado;
+    }
 }
--- PIS UNIDAD II/src/tareas/modelo/tipoTarea.java
+++ PIS UNIDAD II/src/tareas/modelo/tipoTarea.java
@@ -9,5 +9,37 @@ package tareas.modelo;
  * @author ALEJANDRO
  */
 public class tipoTarea {
+    private String nombre;
+    private Integer Id;
+
+    public tipoTarea(Integer Id,String nombre) {
+        this.nombre = nombre;
+
+        this.Id = Id;
+    }
+    public tipoTarea(){
+        
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
     
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return  nombre;
+    }
 }
